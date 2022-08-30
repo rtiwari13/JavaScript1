fd844dbbf183bc044681a8e0c6713a2b949bc29f
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-08-30 10:01:38 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,23 @@
+"use strict";
+// Variables
+// declare a variable
+var nameIs = "MyName";
+
+// use the variable
+ console.log(nameIs);
+
+ //change value
+  nameIs = "YourName"
+  console.log(nameIs);
+
+  // operations
+  var value = 6;
+  console.log(value/2);
+  console.log(value**0.5);
+
+  var $abc = "something"
+  console.log($abc);
+
+  //snake case writing
+  var snake_name = "kingKobra";
+  console.log(snake_name);
