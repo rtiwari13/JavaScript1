03edae564a45a67739706cfacfe48776dcd54dd7
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-11-05 08:16:57 +0530

    Add files via upload

---
+++ dom3.js
@@ -0,0 +1,7 @@
+//select element using querry selector
+const mainHead = document.querySelector("#main-heading");
+console.log(mainHead);
+
+//to select all items we use 
+ const group = document.querySelectorAll(".form-group");
+ console.log(group); // return node
