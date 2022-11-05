ef06598bd4a460032a6c992448a804ad2d15128e
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-11-05 08:18:07 +0530

    Add files via upload

---
+++ dom4.js
@@ -0,0 +1,6 @@
+//change text
+const mainHead = document.getElementById("main-heading");
+console.log(mainHead.textContent); 
+
+mainHead.textContent = "Today is opportunity!";
+console.log(mainHead.textContent);
