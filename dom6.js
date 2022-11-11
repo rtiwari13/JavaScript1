cc49075a89c02e5664504206b4d1c758794c4413
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-11-11 15:12:11 +0530

    Add files via upload

---
+++ dom6.js
@@ -0,0 +1,10 @@
+//get and set attributes
+const kjhhg = document.querySelector("a");
+console.log(kjhhg);
+
+// we can use 'get' to get the value of attributes
+console.log(kjhhg.getAttribute("href"));
+
+//set the value of attributes
+kjhhg.setAttribute("href","nvbhfmbvgvxbzvssmndhs");
+console.log(kjhhg.getAttribute("href"));
