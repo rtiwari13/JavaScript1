935c6bac0d919b0f49adc39d6fe9996b6077e387
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-11-11 15:11:26 +0530

    Delete dom4.js

--- dom4.js
+++
@@ -1,6 +0,0 @@
-//change text
-const mainHead = document.getElementById("main-heading");
-console.log(mainHead.textContent); 
-
-mainHead.textContent = "Today is opportunity!";
-console.log(mainHead.textContent);
