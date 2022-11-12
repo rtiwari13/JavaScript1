06b625f51a840527c7091fc4831e1667eb0ed336
rtiwari13 <110028179+rtiwari13@users.noreply.github.com>
2022-11-12 14:51:34 +0530

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,3 @@
+// toggle will add the class if it does not exist and will remove the class if it is already in existence
+const sectionTodo  = document.querySelector(".section-todo");
+sectionTodo.classList.toggle("bg-dark")
